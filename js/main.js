f61c4730642d6471e8bcff1bdb566aad736a5312
Aleksandr <sklarsanja@gmail.com>
2020-01-06 18:48:54 +0300

    научился делать посты,кнопки и ознакомился с ajax

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+	$('.load-more').on('click',function() {
+		var btn = $(this);
+		var loader = btn.find('span');
+		$.ajax({
+			url: '/data.html',
+			type: 'GET',
+			beforeSend: function() {
+				btn.attr('disabled', 'true');
+				loader.addClass('d-inline-block');
+			},
+			success: function(responce) {
+				setTimeout(function() {
+					loader.removeClass('d-inline-block')
+					btn.attr('disabled', 'false');
+					$('.after-posts').before(responce);
+				}, 1000);
+			},
+			error: function() {
+				alert('Error!');
+			}
+		});
+	});
+});	
